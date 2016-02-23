ca0d5b2a6d5caccb2e8bbc51bc9388681b171b2e
Roger Vuistiner <vuistiner@epsitec.ch>
2016-02-23 17:21:41 +0100

    Opération Bundles: centralise les precompiled headers.

---
+++ pch/stdafx.h
@@ -0,0 +1,24 @@
+// stdafx.h : include file for standard system include files,
+//  or project specific include files that are used frequently, but
+//      are changed infrequently
+//
+
+#pragma once
+#pragma warning(disable:4786)
+
+#include "targetver.h"
+
+#define WIN32_LEAN_AND_MEAN             // Exclude rarely-used stuff from Windows headers
+//	For details about MFC static linking:
+//	http://blogs.msdn.com/b/vcblog/archive/2012/02/06/10263387.aspx
+#define _AFX_NO_MFC_CONTROLS_IN_DIALOGS         // remove support for MFC controls in dialogs
+#define _CRT_SECURE_NO_WARNINGS
+
+#ifndef VC_EXTRALEAN
+#define VC_EXTRALEAN            // Exclude rarely-used stuff from Windows headers
+#endif
+
+#include <afxwin.h>         // MFC core and standard components
+#include <afxext.h>         // MFC extensions
+#include <afxcmn.h>         // MFC common controls
+#include <afxpriv.h>			// MFC support for Windows 95 Common Controls
