1a8f7de4257b9e2035d41ec23cc028085a2de721
Roger Vuistiner <vuistiner@epsitec.ch>
2016-04-20 15:15:56 +0200

    Refine the validation for project type, platform and language in the AddBuildOptions custom task.

--- Tasks/Source/AddBuildOptions.cs
+++ Tasks/Source/AddBuildOptions.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Diagnostics;
 using System.IO;
 using System.Linq;
@@ -12,17 +13,33 @@ namespace Epsitec.Zou
 {
 	public enum Language
 	{
-		None,
+		NotSpecified,
 		Cpp,
 		CSharp
+	}
+	public enum Platform
+	{
+		NotSpecified,
+		AnyCpu,
+		Win32,
+		x86,
+		x64,
+		Arm,
+		Itanium
+	}
+	public enum ProjectType
+	{
+		Sln,
+		CsProj,
+		VcxProj,
 	}
 
 	public class AddBuildOptions : Task
 	{
 		public override bool			Execute()
 		{
-			this.ProjectsOutput = this.Projects.Select (project => project.AddBuildOptions ()).ToArray();
-			return true;
+			this.ProjectsOutput = this.Projects.Select (project => this.AddOptions (project)).ToArray();
+			return !this.Log.HasLoggedErrors;
 		}
 		[Required]
 		public ITaskItem[]				Projects
@@ -35,13 +52,11 @@ namespace Epsitec.Zou
 		{
 			get; private set;
 		}
-	}
-	internal static partial class Mixins
-	{
-		public static ITaskItem				AddBuildOptions(this ITaskItem projectItem)
+
+		private ITaskItem				AddOptions(ITaskItem projectItem)
 		{
 			var project = new TaskItem (projectItem);
-			Mixins.PreprocessOptions (project);
+			this.PreprocessOptions (project);
 
 			var buildItem = new BuildItem ("Project", project);
 			var names = buildItem.CustomMetadataNames.Cast<string> ();
@@ -64,6 +79,111 @@ namespace Epsitec.Zou
 
 			return project;
 		}
+		private void					PreprocessOptions(ITaskItem project)
+		{
+			try
+			{
+				var projectType = project.GetProjectType ();
+				var language    = this.PreprocessLanguage (project, projectType);
+				var platform    = this.PreprocessPlatform (project, projectType, language);
+				language        = this.PreprocessLanguage (project, projectType, language, platform);
+				this.PreprocessOutDir (project, projectType, language, platform);
+			}
+			catch (Exception e)
+			{
+				this.Log.LogErrorFromException (e);
+			}
+		}
+		private Language				PreprocessLanguage(ITaskItem project, ProjectType projectType)
+		{
+			Language language;
+			return ProjectToLanguage.TryGetValue (projectType, out language) ? language : project.GetLanguageMetadata ();
+		}
+		private Platform				PreprocessPlatform(ITaskItem project, ProjectType projectType, Language language)
+		{
+			var platform = project.GetPlatformMetadata ();
+			if (platform == Platform.Win32)
+			{
+				if (language == Language.CSharp)
+				{
+					platform = Platform.AnyCpu;
+				}
+			}
+
+			if (platform == Platform.AnyCpu)
+			{
+				if (projectType == ProjectType.Sln)
+				{
+					project.SetMetadata ("Platform", "Any CPU");
+				}
+				else
+				{
+					project.SetMetadata ("Platform", "AnyCPU");
+				}
+			}
+			return platform;
+		}
+		private Language				PreprocessLanguage(ITaskItem project, ProjectType projectType, Language language, Platform platform)
+		{
+			if (language == Language.NotSpecified)
+			{
+				if (platform == Platform.AnyCpu)
+				{
+					return Language.CSharp;
+				}
+				this.Log.LogError ($"Language cannot be resolved for solution {project.ItemSpec}, please specify:\n  add Language metadata in ImportProject item (exemple: <Language>C#<Language>) \n  or specify it on the command line (exemple: /p:Language=C#)");
+			}
+			return language;
+		}
+		private void					PreprocessOutDir(ITaskItem project, ProjectType projectType, Language language, Platform platform)
+		{
+			if (language == Language.CSharp && string.IsNullOrEmpty(project.GetMetadata ("OutPutPath")))
+			{
+				var outDir = project.GetMetadata ("OutDir");
+				if (!string.IsNullOrEmpty(outDir))
+				{
+					project.SetMetadata ("OutputPath", outDir);
+					project.RemoveMetadata ("OutDir");
+				}
+			}
+		}
+
+		private static readonly Dictionary<ProjectType, Language> ProjectToLanguage = new Dictionary<ProjectType, Language>()
+		{
+			{ ProjectType.CsProj,  Language.CSharp },
+			{ ProjectType.VcxProj, Language.Cpp },
+		};
+	}
+
+	internal class BuildPropertyComparer : IComparer<Tuple<string, string>>
+	{
+		public static readonly BuildPropertyComparer Default = new BuildPropertyComparer ();
+
+		public int							Compare(Tuple<string, string> x, Tuple<string, string> y)
+		{
+			var xEndsWithBackslash = x.Item2.EndsWith ("\\");
+			var yEndsWithBackslash = y.Item2.EndsWith ("\\");
+			if (xEndsWithBackslash)
+			{
+				return yEndsWithBackslash ? string.Compare (x.Item1, y.Item1) : -1;
+			}
+			else if (yEndsWithBackslash)
+			{
+				return xEndsWithBackslash ? string.Compare (x.Item1, y.Item1) : 1;
+			}
+			else
+			{
+				return string.Compare (x.Item1, y.Item1);
+			}
+		}
+	}
+	internal static partial class Mixins
+	{
+
+		public static bool					IsBuildProperty(string name)				=> !Mixins.NonBuildProperties.Contains (name);
+		public static ProjectType			GetProjectType(this ITaskItem item)			=> item.ItemSpec.ToProjectType ();
+		public static Platform				GetPlatformMetadata(this ITaskItem item)	=> item.GetMetadata ("Platform").ToPlatform ();
+		public static Language				GetLanguageMetadata(this ITaskItem item)	=> item.GetMetadata ("Language").ToLanguage ();
 
 		private static IEnumerable<string>	NonBuildProperties
 		{
@@ -75,94 +195,53 @@ namespace Epsitec.Zou
 				yield return "Language";
 			}
 		}
-		private static bool					IsBuildProperty(string name) => !Mixins.NonBuildProperties.Contains (name);
-		private static void					PreprocessOptions(ITaskItem project)
+		private static ProjectType			ToProjectType(this string itemSpec)
 		{
-			Mixins.PreprocessPlatform (project);
-			Mixins.PreprocessOutDir (project);
-		}
-
-
-		private static void					PreprocessPlatform(ITaskItem project)
-		{
-			// Preprocess the 'Any CPU' platform property.
-			// If the item spec is a .sln use 'Any CPU' (with a space).
-			// If the item spec is a .csproj use 'AnyCPU' (without a space).
-			var platform = project.GetMetadata ("Platform");
-			if (!string.IsNullOrEmpty(platform))
+			var extension = Path.GetExtension (itemSpec);
+			ProjectType projectType;
+			if (Enum.TryParse<ProjectType> (extension.Substring (1), true, out projectType))
 			{
-				platform = platform.ToLowerInvariant ();
-				if (platform == "any cpu" || platform == "anycpu")
-				{
-					if (project.ItemSpec.EndsWith (".sln", StringComparison.OrdinalIgnoreCase))
-					{
-						project.SetMetadata ("Platform", "Any CPU");
-					}
-					else
-					{
-						project.SetMetadata ("Platform", "AnyCPU");
-					}
-				}
+				return projectType;
 			}
+			throw new ArgumentOutOfRangeException (nameof (extension), $"Project type '{extension}' not defined in zou");
 		}
-		private static void					PreprocessOutDir(ITaskItem project)
+		private static Platform				ToPlatform(this string value)
 		{
-			if (project.UseOutputPath () && string.IsNullOrEmpty(project.GetMetadata ("OutPutPath")))
+			if (string.IsNullOrWhiteSpace (value))
 			{
-				var outDir = project.GetMetadata ("OutDir");
-				if (!string.IsNullOrEmpty(outDir))
-				{
-					project.SetMetadata ("OutputPath", outDir);
-					project.RemoveMetadata ("OutDir");
-				}
+				return Platform.NotSpecified;
 			}
+			else if (0 == string.Compare(value, "Any CPU", StringComparison.OrdinalIgnoreCase))
+			{
+				return Platform.AnyCpu;
+			}
+			Platform platform;
+			if (Enum.TryParse(value, true, out platform))
+			{
+				return platform;
+			}
+			throw new ArgumentOutOfRangeException (nameof (value), $"Platform '{value}' not defined in zou");
 		}
-		private static bool					UseOutputPath(this ITaskItem item)			=> item.IsCsProj () || item.GetLanguageMetaData () == Language.CSharp;
-		private static bool					IsCsProj(this ITaskItem item)				=> item.ItemSpec.EndsWith (".csproj", StringComparison.OrdinalIgnoreCase);
-		private static Language				GetLanguageMetaData(this ITaskItem item)	=> item.GetMetadata ("Language").ToLanguage ();
 		private static Language				ToLanguage(this string value)
 		{
 			if (string.IsNullOrWhiteSpace (value))
 			{
-				return Language.None;
+				return Language.NotSpecified;
 			}
-			else if (value.StartsWith ("C#", StringComparison.OrdinalIgnoreCase) ||
-				     value.StartsWith ("CSharp", StringComparison.OrdinalIgnoreCase))
+			else if (0 == string.Compare (value, "C#", StringComparison.OrdinalIgnoreCase))
 			{
 				return Language.CSharp;
 			}
-			else if (value.StartsWith ("C++", StringComparison.OrdinalIgnoreCase) ||
-				     value.StartsWith ("Cpp", StringComparison.OrdinalIgnoreCase))
+			else if (0 == string.Compare (value, "C++", StringComparison.OrdinalIgnoreCase))
 			{
 				return Language.Cpp;
 			}
-			else
+			Language language;
+			if (Enum.TryParse (value, true, out language))
 			{
-				throw new ArgumentOutOfRangeException (nameof (value), $"Language '{value}' not defined in zou");
+				return language;
 			}
-		}
-
-		private class BuildPropertyComparer : IComparer<Tuple<string, string>>
-		{
-			public static readonly BuildPropertyComparer Default = new BuildPropertyComparer ();
-
-			public int Compare(Tuple<string, string> x, Tuple<string, string> y)
-			{
-				var xEndsWithBackslash = x.Item2.EndsWith ("\\");
-				var yEndsWithBackslash = y.Item2.EndsWith ("\\");
-				if (xEndsWithBackslash)
-				{
-					return yEndsWithBackslash ? string.Compare (x.Item1, y.Item1) : -1;
-				}
-				else if (yEndsWithBackslash)
-				{
-					return xEndsWithBackslash ? string.Compare (x.Item1, y.Item1) : 1;
-				}
-				else
-				{
-					return string.Compare (x.Item1, y.Item1);
-				}
-			}
+			throw new ArgumentOutOfRangeException (nameof (value), $"Language '{value}' not defined in zou");
 		}
 	}
 }
