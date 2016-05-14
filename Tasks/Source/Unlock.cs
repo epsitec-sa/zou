183b7202e2f70f8b38536c8bba7418e747e78b5e
Roger Vuistiner <vuistiner@epsitec.ch>
2016-05-14 02:38:38 +0200

    Tasks: fix mutual exclusion tasks.

--- Tasks/Source/Unlock.cs
+++ Tasks/Source/Unlock.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.AccessControl;
 using System.Text;
 using System.Text.RegularExpressions;
 using System.Threading;
@@ -22,12 +23,16 @@ namespace Epsitec.Zou
 			try
 			{
 				// Replace consecutive non-alphanumeric characters with underscore.
-				var mutexName = Regex.Replace (this.Name, @"[^\w]+", "_", RegexOptions.CultureInvariant);
+				var mutexName = Regex.Replace (this.Name.ToLowerInvariant (), @"[^\w]+", "_", RegexOptions.CultureInvariant);
 				Mutex mutex;
-				if (Mutex.TryOpenExisting (mutexName, out mutex))
+				if (Mutex.TryOpenExisting (mutexName, MutexRights.Modify | MutexRights.Synchronize, out mutex))
 				{
 					mutex.ReleaseMutex ();
-					mutex.Dispose ();
+					this.Log.LogMessageFromText ($"Unlock -> exited mutex: {mutexName}", MessageImportance.Normal);
+				}
+				else
+				{
+					this.Log.LogMessageFromText ($"Unlock -> unable to open mutex: {mutexName}", MessageImportance.Normal);
 				}
 			}
 			catch (Exception e)
