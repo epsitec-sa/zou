354dece65309018c2b7c580cead114ee38f2483b
Jonny Quarta <quarta@epsitec.ch>
2016-07-22 11:12:29 +0200

    Fix git_config.bat file

--- bin/git_config.bat
+++ bin/git_config.bat
@@ -3,8 +3,8 @@
 git config --global status.submoduleSummary true
 git config --global diff.submodule log
 git config --global alias.sdiff '!'"git diff && git submodule foreach 'git diff'"
-git config --global alias.spull '!git pull && git submodule sync --recursive && git submodule update --init --recursive'
-git config --global alias.spush 'push --recurse-submodules=on-demand'
-git config --global alias.sclean '!'"git clean -xdf && git submodule foreach --recursive 'git clean -xdf'"
-git config --global alias.oprune 'fetch origin --prune'
-git config --global alias.supdate 'submodule update --init --recursive'
+git config --global alias.spull '!'"git pull && git submodule sync --recursive && git submodule update --init --recursive"
+git config --global alias.spush "push --recurse-submodules=on-demand"
+git config --global alias.sclean '!'"git clean -xdf && git submodule foreach --recursive 'git clean -xdf"
+git config --global alias.oprune "fetch origin --prune"
+git config --global alias.supdate "submodule update --init --recursive"
