ba0bd733e11dfe79bd72fb14826c61fa3fdc8aa4
Roger Vuistiner <vuistiner@epsitec.ch>
2016-09-28 13:27:57 +0200

    Standardize common precompiled header.
    - adapt to MFC wizard's generated precompiled header.
    - warn usage of _AFX_NO_MFC_CONTROLS_IN_DIALOGS macro.

--- pch/stdafx.h
+++ pch/stdafx.h
@@ -6,19 +6,47 @@
 #pragma once
 #pragma warning(disable: 4503 4786)
 
+// Exclude rarely-used stuff from Windows headers
+#ifndef VC_EXTRALEAN
+#define VC_EXTRALEAN            
+#endif
+#ifndef WIN32_LEAN_AND_MEAN
+#define WIN32_LEAN_AND_MEAN            
+#endif
+
 #include "targetver.h"
 
-#define WIN32_LEAN_AND_MEAN             // Exclude rarely-used stuff from Windows headers
-//	For details about MFC static linking:
-//	http://blogs.msdn.com/b/vcblog/archive/2012/02/06/10263387.aspx
-#define _AFX_NO_MFC_CONTROLS_IN_DIALOGS         // remove support for MFC controls in dialogs
 #define _CRT_SECURE_NO_WARNINGS
+// désactive le masquage MFC de certains messages d'avertissement courants et par ailleurs souvent ignorés
+#define _AFX_ALL_WARNINGS
 
-#ifndef VC_EXTRALEAN
-#define VC_EXTRALEAN            // Exclude rarely-used stuff from Windows headers
-#endif
+//	For details about MFC static linking
+//	http://blogs.msdn.com/b/vcblog/archive/2012/02/06/10263387.aspx
+//  WARNING: all static libraries linked together should [un]define the following macro to avoid link errors
+#define _AFX_NO_MFC_CONTROLS_IN_DIALOGS         // remove support for MFC controls in dialogs
 
 #include <afxwin.h>         // MFC core and standard components
 #include <afxext.h>         // MFC extensions
-#include <afxcmn.h>         // MFC common controls
-#include <afxpriv.h>			// MFC support for Windows 95 Common Controls
+
+#include <afxdisp.h>        // MFC Automation
+
+#ifndef _AFX_NO_OLE_SUPPORT
+#include <afxdtctl.h>           // Prise en charge MFC pour les contrôles communs Internet Explorer 4
+#endif
+#ifndef _AFX_NO_AFXCMN_SUPPORT
+#include <afxcmn.h>             // Prise en charge des MFC pour les contrôles communs Windows
+#endif // _AFX_NO_AFXCMN_SUPPORT
+
+#include <afxpriv.h>		// MFC support for Windows 95 Common Controls
+//#include <afxcontrolbars.h>     // prise en charge des MFC pour les rubans et les barres de contrôles
+
+
+#ifdef _UNICODE
+#if defined _M_IX86
+#pragma comment(linker,"/manifestdependency:\"type='win32' name='Microsoft.Windows.Common-Controls' version='6.0.0.0' processorArchitecture='x86' publicKeyToken='6595b64144ccf1df' language='*'\"")
+#elif defined _M_X64
+#pragma comment(linker,"/manifestdependency:\"type='win32' name='Microsoft.Windows.Common-Controls' version='6.0.0.0' processorArchitecture='amd64' publicKeyToken='6595b64144ccf1df' language='*'\"")
+#else
+#pragma comment(linker,"/manifestdependency:\"type='win32' name='Microsoft.Windows.Common-Controls' version='6.0.0.0' processorArchitecture='*' publicKeyToken='6595b64144ccf1df' language='*'\"")
+#endif
+#endif
