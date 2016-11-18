71ba800e169603f576b94faa28ef7528098f1318
Roger Vuistiner <vuistiner@epsitec.ch>
2016-11-18 12:08:28 +0100

    Tasks: add an OutputItems property to SaveItems task.
    - returns all the items in the new file.

--- Tasks/Source/Items/SaveItems.cs
+++ Tasks/Source/Items/SaveItems.cs
@@ -31,35 +31,45 @@ namespace Epsitec.Zou
 			get;
 			set;
 		}
+		[Output]
+		public ITaskItem[]		OutputItems
+		{
+			get;
+			set;
+		}
 		public override bool	Execute()
 		{
-			var currentMemos = this.Items
-				.Select (item => ItemMemo.FromTaskItem (item, this.RelativeTo, this.Metadata))
-				.ToArray ();
+			var inputMemos   = this.Items.Select (item => ItemMemo.FromTaskItem (item, this.RelativeTo, this.Metadata)).ToArray ();
+			var outputMemos  = this.GetOuptutMemos (inputMemos);
+			this.OutputItems = outputMemos.Select (m => m.ToTaskItem (this.RelativeTo)).ToArray ();
 
-			if (this.Overwrite)
-			{
-				System.IO.File.WriteAllLines (this.File, currentMemos.Serialize ().ToArray ());
-			}
-			else if (this.KeepDuplicates)
-			{
-				System.IO.File.AppendAllLines (this.File, currentMemos.Serialize ().ToArray ());
-			}
-			else
-			{
-				var previousMemos = ItemMemo
-					.Parse (this.ReadAllLines ())
-					.ToArray ();
+			System.IO.File.WriteAllLines (this.File, outputMemos.Serialize ().ToArray ());
 
-				var commonMemos = currentMemos.Join    (previousMemos, c => c.Id, p => p.Id, (c, p) => c.MergeMetadata (p), StringComparer.OrdinalIgnoreCase);
-				var newMemos    = currentMemos.Except  (commonMemos, m => m.Id, StringComparer.OrdinalIgnoreCase);
-				var oldMemos    = previousMemos.Except (commonMemos, m => m.Id, StringComparer.OrdinalIgnoreCase);
-
-				System.IO.File.WriteAllLines (this.File, newMemos.Concat (commonMemos).Concat (oldMemos).Serialize ().ToArray ());
-
-			}
 			return !this.Log.HasLoggedErrors;
 		}
 
+		private ItemMemo[]		GetOuptutMemos(ItemMemo[] inputMemos)
+		{
+			if (this.Overwrite)
+			{
+				return inputMemos;
+			}
+			else
+			{
+				var previousMemos = ItemMemo.Parse (this.ReadAllLines ()).ToArray ();
+				if (this.KeepDuplicates)
+				{
+					return inputMemos.Concat (previousMemos).ToArray ();
+				}
+				else
+				{
+					var commonMemos = inputMemos.Join (previousMemos, c => c.Id, p => p.Id, (c, p) => c.MergeMetadata (p), StringComparer.OrdinalIgnoreCase);
+					var newMemos    = inputMemos.Except (commonMemos, m => m.Id, StringComparer.OrdinalIgnoreCase);
+					var oldMemos    = previousMemos.Except (commonMemos, m => m.Id, StringComparer.OrdinalIgnoreCase);
+
+					return newMemos.Concat (commonMemos).Concat (oldMemos).ToArray ();
+				}
+			}
+		}
 	}
 }
