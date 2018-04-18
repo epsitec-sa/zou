535707bb3202123f7f0c66d19d812451d7f897dc
Roger Vuistiner <vuistiner@epsitec.ch>
2018-04-18 12:06:45 +0200

    Work out .net core project nesting.

---
+++ Tasks/Source/DebuggerEx.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Epsitec.Zou
+{
+    public static class DebuggerEx
+    {
+        public static void WaitAttached()
+        {
+            Console.WriteLine($"Attach to MSBuild (PID = {System.Diagnostics.Process.GetCurrentProcess().Id})");
+            while (!System.Diagnostics.Debugger.IsAttached) ;
+        }
+    }
+}
