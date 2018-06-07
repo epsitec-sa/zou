46705e6f73a285d725ece91d7f905b46f756a38f
Roger Vuistiner <vuistiner@epsitec.ch>
2018-06-07 17:14:18 +0200

    Add git prunetags command.

--- bin/git_config.sh
+++ bin/git_config.sh
@@ -10,6 +10,7 @@ git config --global alias.oprune 'fetch origin --prune'
 git config --global alias.supdate 'submodule update --init --recursive'
 git config --global alias.issue '!f() { git checkout -b issue/$1 master && git push -u origin issue/$1; }; f'
 git config --global alias.smaster '!'"git checkout master && git pull && git submodule foreach 'git checkout master && git pull'"
+git config --global alias.prunetags '!'"git tag -l | xargs git tag -d && git fetch -t"
 
 # Semantic versioning
 git config --global --remove-section versionsort >/dev/null 2>&1
