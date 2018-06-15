5e7d51f62c0a1868c474b2cd89e752dccfb13167
Roger Vuistiner <vuistiner@epsitec.ch>
2018-06-15 10:32:02 +0200

    git: work out git aliases.
    - add reset and copy options to git_config scripts
    - add for and sfor aliases
    - add curbranch alias
    - add foldtags, otags & foldotags aliases
    - enhance vtable alias (make it recursive)

--- .Documentation/GitAliases.md
+++ .Documentation/GitAliases.md
@@ -4,32 +4,46 @@ Git aliases can be enabled by running `zou/bin/git_config[.bat|.sh]`
 
 | Alias | Parameters | Description
 |:-|:-|:-
+| git **bundle-dir** | [*FOLDER*] | display the parent bundle directory (see **module-id**)
+| git **curbranch** | [*FOLDER*] | display the current branch 
+| git **deltag** | *TAG* | delete a tag locally and remotely (see **newtag**, **mvtag**)
+| git **foldtags** | *SUFFIX* | fold suffixed tags in suffixed folder (`v2.0.0-server -> server/v2.0.0`)
+| git **foldotags** | *SUFFIX* | fold non `semver` tags to `other` folder (`tag -> other/tag`) (see **otags**)
+| git **for** | **'** *COMMAND* **'** | execute given command in bundle and 1st level sub-modules (`git for 'git vmax'`)
+| git **for-q** | **'** *COMMAND* **'** | execute given command in bundle and 1st level sub-modules -- quiet
+| git **for-qr** | **'** *COMMAND* **'** | execute given command in bundle and all sub-modules --quiet -- recursive
+| git **for-r** | **'** *COMMAND* **'** | execute given command in bundle and all sub-modules -- recursive
+| git **issue** | *DESCR* | create an issue branch (`git issue 305-buggy` -> *issue/305-buggy**)
+| git **module-id** | [*FOLDER*] | display the module ID (relative path to bundle directory) of the given module folder (see **bundle-dir**)
+| git **mvbranch** | *OLD NEW* | rename a branch locally and remotely
+| git **mvtag** | *OLD NEW* | rename a tag locally and remotely (see **deltag**, **newtag**)
+| git **newtag** | *TAG* | create a local tag and push it on origin (see **deltag**, **mvtag**)
+| git **prunetags** || prune local tags no longer present on origin
 | git **oprune** || prune tracking branches no longer present on origin
+| git **otags** || display non `semver` tags (see **foldotags**)
+| git **sclean** || remove all untracked files
 | git **sdiff** ||
+| git **sfor** | **'** *COMMAND* **'** | execute given command in 1st level sub-modules
+| git **sfor-q** | **'** *COMMAND* **'** | execute given command in 1st level sub-modules -- quiet
+| git **sfor-qr** | **'** *COMMAND* **'** | execute given command in all sub-modules --quiet -- recursive
+| git **sfor-r** | **'** *COMMAND* **'** | execute given command in all sub-modules -- recursive
+| git **smaster** || update master snapshot by updating (checkout + pull) master branches of registered submodules
 | git **spull** ||
 | git **spush** ||
-| git **sclean** || remove all untracked files
 | git **supdate** || for each registered submodules, clone missing ones and update their working trees
-| git **smaster** || update master snapshot by updating (checkout + pull) master branches of registered submodules
-| git **issue** | *DESCR* | create an issue branch (`git issue 305-buggy` -> *issue/305-buggy**)
-| git **mvbranch** | *OLD NEW* | rename a branch locally and remotely
-| git **prunetags** || prune local tags no longer present on origin
-| git **tags** || list local and remote tags
-| git **newtag** | *TAG* | create a local tag and push it on origin
-| git **deltag** | *TAG* | delete a tag locally and remotely
 | git **tag2hash** | *TAG* | display short hash of given tag
-| git **mvtag** | *OLD NEW* | rename a tag locally and remotely
+| git **tags** || list local and remote tags
 | git **vbranch** | *MAJOR.MINOR* | create a `vbranch` and a `vnode` (*1.0 <- v1.0-@*)
-| git **vmin** | *[MAJOR[.MINOR]]* | display minimum version using given pattern
-| git **vmax** | *[MAJOR[.MINOR]]* | display maximum version using given pattern
-| git **vtags** || list local `vtag`s
-| git **vcommit2tag** | [*HASH*] | display the most recent `vtag` description (1) reachable from given `vcommit`
+| git **vcheckout** | [*HASH*] | checkout the `vbranch` of the given `vcommit`
 | git **vcommit2major** | [*HASH*] | display the most recent `MAJOR` version reachable from given `vcommit`
 | git **vcommit2minor** | [*HASH*] | display the most recent `MAJOR.MINOR` version reachable from given `vcommit`
-| git **vcheckout** | [*HASH*] | checkout the `vbranch` of the given `vcommit`
-| git **vminor** | [*HASH*] | checkout the `vbranch` having the same `MAJOR.MINOR` version as the given `vcommit`
+| git **vcommit2tag** | [*HASH*] | display the most recent `vtag` description (1) reachable from given `vcommit`
 | git **vmajor** | [*HASH*] | checkout the `vbranch` having the same `MAJOR` and greatest `MINOR` version as the given `vcommit`
+| git **vmax** | *[MAJOR[.MINOR]]* | display maximum version using given pattern
+| git **vminor** | [*HASH*] | checkout the `vbranch` having the same `MAJOR.MINOR` version as the given `vcommit`
+| git **vmin** | *[MAJOR[.MINOR]]* | display minimum version using given pattern
 | git **vnext** | [*HASH*] | checkout the `vbranch` with the greatest `MAJOR` and `MINOR` version
 | git **vtable** || create a markdown table of sub-module versions (`versions.md`)
+| git **vtags** || list local `vtag`s
 
 (1) `vtag` description exemple : `v1.0.0-2-gc8af267` (see [git describe](https://git-scm.com/docs/git-describe))
--- bin/git_config.sh
+++ bin/git_config.sh
@@ -1,32 +1,68 @@
 #!/bin/sh
 
+# Parse
+while [[ $# -gt 0 ]]; do
+	case $1 in
+		-h )
+			echo
+			echo 'Usage:'
+			echo "  $0 [OPTIONS]"
+			echo
+			echo 'Options:'
+			echo '  -h		Help'
+			echo '  -r		Reset aliases'
+			echo "  -c		Create a copy to $(echo $USERPROFILE | sed 's,\\,/,g')/.gitconfig.bash.zou"
+			exit 0
+			;;
+		-r ) reset=true; shift;;
+		-c ) copy=true; shift;;
+	esac
+done
+
 git config --global status.submoduleSummary true
 git config --global diff.submodule log
 
 # Aliases
-#git config --global --remove-section alias >/dev/null 2>&1
+[ "$reset" == 'true' ] && git config --global --remove-section alias >/dev/null 2>&1
+
 git config --global alias.oprune 'fetch origin --prune'
 # Sub-modules
-git config --global alias.sdiff '!'"git diff && git submodule foreach 'git diff'"
+git config --global alias.bundle-dir '!f() { r=${1:-`pwd -P`}; while [[ "$r" != '"'"''"'"' && ! -d "$r/.git" ]]; do r=${r%/*}; done; echo $r; }; f'
+git config --global alias.module-id  '!f() { r=${1:-`pwd -P`}; c=$r; while [[ "$r" != '"'"''"'"' && ! -d "$r/.git" ]]; do r=${r%/*}; done; id=${c#$r}; id=${id#/}; echo ${id%/}; }; f'
+git config --global alias.sfor 'submodule foreach'
+git config --global alias.sfor-q 'submodule --quiet foreach'
+git config --global alias.sfor-r 'submodule foreach -- recursive'
+git config --global alias.sfor-qr 'submodule --quiet foreach -- recursive'
+# Bundle and sub-modules
+git config --global alias.for '!path=$(basename `pwd`); eval $@; git submodule foreach'
+git config --global alias.for-r '!path=$(basename `pwd`); eval $@; git submodule foreach --recursive'
+git config --global alias.for-q '!path=$(basename `pwd`); eval $@; git submodule --quiet foreach'
+git config --global alias.for-qr '!path=$(basename `pwd`); eval $@; git submodule --quiet foreach -- recursive'
+
+git config --global alias.sdiff '!git diff && git submodule foreach '"'git diff'"
 git config --global alias.spull '!git pull && git submodule sync --recursive && git submodule update --init --recursive'
 git config --global alias.spush 'push --recurse-submodules=on-demand'
-git config --global alias.sclean '!'"[ -d node_modules ] && rm -rf node_modules; git clean -xdf -e packages && git submodule foreach --recursive '[ -d node_modules ] && rm -rf node_modules; git clean -xdf'"
+git config --global alias.sclean "![ -d node_modules ] && rm -rf node_modules; git clean -xdf -e packages && git submodule foreach --recursive '[ -d node_modules ] && rm -rf node_modules; git clean -xdf'"
 git config --global alias.supdate 'submodule update --init --recursive'
 # Branch
 git config --global alias.issue '!f() { git checkout -b issue/$1 master && git push -u origin issue/$1; }; f'
-git config --global alias.smaster '!'"git checkout master && git pull && git submodule foreach 'git checkout master && git pull'"
+git config --global alias.smaster '!git checkout master && git pull && git submodule foreach '"'git checkout master && git pull'"
 # rename local + remote branch 
-git config --global alias.mvbranch '!'"f() { local old=$1; local new=$2; git ls-remote --heads --exit-code origin $old >/dev/null || git push origin $old; git branch $new origin/$old >/dev/null 2>&1; git push origin --set-upstream $new >/dev/null 2>&1; git push --delete --force origin $old >/dev/null 2>&1; git branch -D $old >/dev/null 2>&1; }; f"
+git config --global alias.mvbranch '!f() { local old=$1; local new=$2; git ls-remote --heads --exit-code origin $old >/dev/null || git push origin $old; git branch $new origin/$old >/dev/null 2>&1; git push origin --set-upstream $new >/dev/null 2>&1; git push --delete --force origin $old >/dev/null 2>&1; git branch -D $old >/dev/null 2>&1; }; f'
+git config --global alias.curbranch '!c=$(git rev-parse --abbrev-ref HEAD); [ $c == '"'"'HEAD'"'"' ] && git rev-parse --short HEAD || echo $c'
 
 # Tags
-git config --global alias.prunetags '!'"git tag -l | xargs git tag -d && git fetch -t"
-git config --global alias.tags '!'"git tag; git ls-remote --tags origin | sed s/[[:alnum:]]*[[:space:]]*//"
-git config --global alias.newtag '!'"f() { git tag $1 $2; git push origin $1; }; f"
-git config --global alias.deltag '!'"f() { git tag --delete $1; git push --delete origin $1; }; f"
-:: get short hash of given tag
-git config --global alias.tag2hash '!'"f() { git describe --tags --long $1 | sed s,.*-[0-9]*-g,,g; }; f"
-:: rename local + remote tag 
-git config --global alias.mvtag '!'"f() { local old=$1; local new=$2; local hash=$(git tag2hash $old); git deltag $old; git newtag $new $tag2hash; }; f"
+git config --global alias.prunetags '!git tag -l | xargs git tag -d && git fetch -t'
+git config --global alias.foldtags '!f() { local suffix=$1; for t in $(git tag | grep -$suffix$); do st=$(echo $t | sed s,-$suffix$,,I); git mvtag $t $suffix/$st ; done; }; f'
+git config --global alias.otags '!git tag | grep -Ev '"'"'^v[0-9]+\.[0-9]+\.[0-9]+(-(@|(alpha|beta|rc)[0-9]{2}))?$'"'"' | grep -Ev '"'"'^[^/]+/.+$'"'"''
+git config --global alias.foldotags '!for t in $(git otags); do git mvtag $t other/$t; done'
+git config --global alias.tags '!git tag; git ls-remote --tags origin | sed s,[[:alnum:]]*[[:space:]]*,,'
+git config --global alias.newtag '!f() { git tag $1 $2; git push origin $1; }; f'
+git config --global alias.deltag '!f() { git tag --delete $1; git push --delete origin $1; }; f'
+# get short hash of given tag
+git config --global alias.tag2hash '!f() { git describe --tags --long $1 | sed s,.*-[0-9]*-g,,g; }; f'
+# rename local + remote tag 
+git config --global alias.mvtag '!f() { local old=$1; local new=$2; local hash=$(git tag2hash $old); git deltag $old; git newtag $new $hash; }; f'
 
 # Semantic versioning
 git config --global --remove-section versionsort >/dev/null 2>&1
@@ -35,18 +71,20 @@ git config --global --add versionsort.suffix -alpha
 git config --global --add versionsort.suffix -beta
 git config --global --add versionsort.suffix -rc
 
-:: VBranch
-git config --global alias.vbranch '!'"f() { git checkout -b $1 && git tag v$1-@; }; f"
-:: VTag
-git config --global alias.vmin '!'"f() { local version=$1; local regex; if [[ -z \"$version\" ]]; then regex=[0-9]; else regex=$(echo $version | sed s,[.],\\.,g); fi; git tag -l --sort=v:refname | grep -m1 ^v$regex; }; f"
-git config --global alias.vmax '!'"f() { local version=$1; local regex; if [[ -z \"$version\" ]]; then regex=[0-9]; else regex=$(echo $version | sed s,[.],\\.,g); fi; git tag -l --sort=-v:refname | grep -m1 ^v$regex; }; f"
-git config --global alias.vtags '!'"f() { local version=$1; local regex; if [[ -z \"$version\" ]]; then regex=[0-9]; else regex=$(echo $version | sed s,[.],\\.,g); fi; git tag -l --sort=-v:refname | grep ^v$regex; }; f"
+# VBranch
+git config --global alias.vbranch '!f() { git checkout -b $1 && git tag v$1-@; }; f'
+# VTag
+git config --global alias.vmin '!f() { local version=$1; local regex; if [[ -z "$version" ]]; then regex=[0-9]; else regex=$(echo $version | sed s,[.],\\.,g); fi; git tag -l --sort=v:refname | grep -m1 ^v$regex || true; }; f'
+git config --global alias.vmax '!f() { local version=$1; local regex; if [[ -z "$version" ]]; then regex=[0-9]; else regex=$(echo $version | sed s,[.],\\.,g); fi; git tag -l --sort=-v:refname | grep -m1 ^v$regex || true; }; f'
+git config --global alias.vtags '!f() { local version=$1; local regex; if [[ -z "$version" ]]; then regex=[0-9]; else regex=$(echo $version | sed s,[.],\\.,g); fi; git tag -l --sort=-v:refname | grep ^v$regex || true; }; f'
 
-git config --global alias.vcommit2tag '!'"f() { git describe --tags --match 'v[0-9]*' $1; }; f"
-git config --global alias.vcommit2major '!'"f() { git vcommit2tag $1 |  sed -r 's,^v([0-9]+).*,\1,'; }; f"
-git config --global alias.vcommit2minor '!'"f() { git vcommit2tag $1 |  sed -r 's,^v([0-9]+\.[0-9]+).*,\1,'; }; f"
-git config --global alias.vcheckout '!'"f() { git vcommit2minor $1 | xargs git checkout >/dev/null; }; f"
-git config --global alias.vmajor '!'"f() { git vcommit2major $1 | xargs git vmax | xargs git vcheckout; }; f"
-git config --global alias.vminor '!'"f() { git vcommit2minor $1 | xargs git vmax | xargs git vcheckout; }; f"
-git config --global alias.vnext '!'"f() { git vmax $1 | xargs git vcheckout; }; f"
-git config --global alias.vtable '!'"echo / Module / Version / Delta >_v1; echo /:-/:-/:- >>_v1; git submodule foreach 'echo / $path / $(git vcommit2tag | sed -r \"s,(.*)-g[[:alnum:]]*$,\1,\" | sed -r \"s,(.*)-([0-9]+),\1 / \2,\") >>../_v2'; sort _v2 >> _v1; sed -r 's,/,|,g' _v1 >versions.md; rm _v?"
+git config --global alias.vcommit2tag '!f() { git describe --tags --match '"'"'v[0-9]*'"'"' $1 2>/dev/null || true; }; f'
+git config --global alias.vcommit2major '!f() { git vcommit2tag $1 |  sed -E '"'"'s,^v([0-9]+).*,\1,'"'"'; }; f'
+git config --global alias.vcommit2minor '!f() { git vcommit2tag $1 |  sed -E '"'"'s,^v([0-9]+\.[0-9]+).*,\1,'"'"'; }; f'
+git config --global alias.vcheckout '!f() { git vcommit2minor $1 | xargs git checkout >/dev/null; }; f'
+git config --global alias.vmajor '!f() { git vcommit2major $1 | xargs git vmax | xargs git vcheckout; }; f'
+git config --global alias.vminor '!f() { git vcommit2minor $1 | xargs git vmax | xargs git vcheckout; }; f'
+git config --global alias.vnext '!f() { git vmax $1 | xargs git vcheckout; }; f'
+git config --global alias.vtable '!row() { echo _ $1 _ $(git curbranch) _ $((git vcommit2tag 2>/dev/null || true) | sed -E '"'"'s,(.*)-g[[:alnum:]]*$,\1,'"'"' | sed -E '"'"'s,(.*)-([0-9]+)$,\1 _ \2,'"'"'); }; export -f row; export v1=`pwd`/_v1; export v2=`pwd`/_v2; echo _ Module _ Head _ Version _ Delta >$v1; echo _:-_:-_:-_:- >>$v1; row $(basename `pwd`) >>$v1; git submodule foreach --recursive '"'"'row $(git module-id) >>$v2'"'"'; sort $v2 >>$v1; sed -E '"'"'s,_,|,g'"'"' $v1 >versions.md; rm $v1 $v2'
+
+[ "$copy" == 'true' ] && cp -f "$USERPROFILE/.gitconfig" "$USERPROFILE/.gitconfig.bash.zou"
