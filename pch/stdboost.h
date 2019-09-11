a67790b854da19cf5b0f2702f876b2c4a1379898
Roger Vuistiner <vuistiner@epsitec.ch>
2019-09-11 16:04:20 +0200

    boost: enahnce boost precompiled header.

--- pch/stdboost.h
+++ pch/stdboost.h
@@ -8,6 +8,12 @@
 
 #pragma once
 
+#pragma warning(push)
+#pragma warning(disable : 26439) // This kind of function may not throw. Declare it 'noexcept'.
+#ifdef _WIN32
+#pragma warning(disable : 4244) // disable "conversion....possible loss of data" warning
+#endif
+
 #include <boost/algorithm/string.hpp>
 #include <boost/algorithm/string/predicate.hpp>
 #include <boost/archive/iterators/binary_from_base64.hpp>
@@ -16,9 +22,11 @@
 #include <boost/asio.hpp>
 #include <boost/asio/spawn.hpp>
 #include <boost/date_time.hpp>
+#include <boost/exception/exception.hpp>
 #include <boost/filesystem.hpp>
 #include <boost/filesystem/path.hpp>
 #include <boost/format.hpp>
+#include <boost/function/function_base.hpp>
 #include <boost/functional/hash.hpp>
 #include <boost/lexical_cast.hpp>
 #include <boost/locale.hpp>
@@ -34,8 +42,6 @@
 #include <boost/uuid/uuid_generators.hpp>
 #include <boost/uuid/uuid_io.hpp>
 
-// Disable some boost 1.70.0 warnings
-#pragma warning(push)
-#pragma warning(disable : 4244) // disable "conversion....possible loss of data" warning
 #include <boost/process/detail/windows/wait_group.hpp>
+
 #pragma warning(pop)
