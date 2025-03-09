6b6a22240b6721367d81bc6aa48cf9f0d71b6229
Roger Vuistiner <vuistiner@epsitec.ch>
2025-03-09 16:53:37 +0100

    NuPkg: update zou-pkg.bat to save generated JSON files in the obj directory.

--- NuPkg/zou-pkg.bat
+++ NuPkg/zou-pkg.bat
@@ -4,8 +4,8 @@ for %%x in (net6 net8 net9) do (
   echo [0;1;4mRestoring [92m%%x[37m packages[0m
   
   dotnet restore NuPkg.csproj --tl:off /p:CoreTargetFramework=%%x
-  dotnet list NuPkg.csproj package --format json >pkg-%%x.json
-  echo   Generated resolved versions listing -^> [92mpkg-%%x.json[0m
-  dotnet list NuPkg.csproj package --format json --outdated >pkg-%%x-outdated.json
-  echo   Generated outdated versions listing -^> [92mpkg-%%x-outdated.json[0m
+  dotnet list NuPkg.csproj package --format json >obj/pkg-%%x.json
+  echo   Generated resolved versions listing -^> [92mobj/pkg-%%x.json[0m
+  dotnet list NuPkg.csproj package --format json --outdated >obj/pkg-%%x-outdated.json
+  echo   Generated outdated versions listing -^> [92mobj/pkg-%%x-outdated.json[0m
 )
